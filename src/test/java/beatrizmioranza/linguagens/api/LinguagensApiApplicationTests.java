6627341bea74b958ba018e9f1bf30993ce717af3
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-07-23 13:24:41 -0300

    feat: modelando as rotas

---
+++ src/test/java/beatrizmioranza/linguagens/api/LinguagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package beatrizmioranza.linguagens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
